307bd316399c0f57c0dda920ce2bf7288c62bc22
timerg <ggteven@gmail.com>
2018-04-15 20:10:08 +0800

    Switch from anychart to Cnvas

--- ReadSerial.js
+++ ReadSerial.js
@@ -1,32 +1,59 @@
-// SocketiO server
+
+// Serial Port
 var SerialPort = require('serialport');
+const ByteLength = SerialPort.parsers.ByteLength;
+// SocketiO server
 var express = require('express')
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+// SocketiO server
 server.listen(3000);
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/interface/interface.html');
 });
 app.use(express.static(__dirname + '/'));
 
-
 io.on('connection', function (socket) {
   socket.on('start', function () {
-    console.log("hey You");
+    port.open();
+  });
+  socket.on('stop', function () {
+    port.close();
   });
 });
 
 // Serial Port
 
-var port = new SerialPort('/dev/tty.usbserial-A105N9Y1', {
-  baudRate: 9600
+const dev = "/dev/tty.usbserial-A105N9Y1";
+
+const port = new SerialPort(dev, {
+  baudRate: 9600,
+  autoOpen: false
 });
+const parser = port.pipe(new ByteLength({length: 1280}));
+
+
+
 port.on('error', function(err) {
   console.log('Error: ', err.message);
 })
 
-port.on('data', function (data) {
-  console.log('Data:', data);
-});
+port.on('open', () => {
+  console.log("Port Open");
+  parser.on('data', (buffer) => {
+    var arr = Array.prototype.slice.call(buffer, 0)
+    // console.log("Parser On");
+    console.log(buffer[0]);
+    // for(var i = 0; i < 30; i++){
+      io.emit('data', arr);
+    // }
+  });
+})
+
+port.on('close', () => {
+  console.log("Port Close");
+})
+
+
--- interface/interface.js
+++ interface/interface.js
@@ -2,33 +2,22 @@
 
 var socket = io();
 
-
+var xval = 0;
 
 
 anychart.onDocumentReady(function () {
-
   // data
   dataSet = anychart.data.set([
-    {x: "P1", value: 100},
-    {x: "P2", value: 200},
-    {x: "P3", value: 15},
-    {x: "P4", value: 130},
-    {x: "P5", value: 153},
-    {x: "P6", value: 120},
-    {x: "P7", value: 151},
-    {x: "P8", value: 58},
-    {x: "P9", value: 19},
-    {x: "P10", value: 135},
-    {x: "P11", value: 170},
-    {x: "P12", value: 195},
-    {x: "P13", value: 22},
-    {x: "P14", value: 175},
-    {x: "P15", value: 120}
+    {x: 0, value: 100},
   ]);
-
+  for(var i = 0; i < 1280; i++){
+    dataSet.append({
+      x: 0,
+      value: 0
+    })
+  }
   // set chart type
   var chart = anychart.line();
-
   chart.title().text("Click on Chart to Add a Point ");
 
   // set data
@@ -44,6 +33,21 @@ anychart.onDocumentReady(function () {
   indexSetter = (dataSet.mapAs().getRowsCount())+1;
 });
 
+
+socket.on('data', function (buffer){
+    var rows = new Array(1280);
+    var row;
+    for(var i = 0; i < 1280; i++){
+      rows[i] = {
+        x: xval++,
+        value: buffer[i]
+      };
+      // dataSet.remove(0);
+    }
+    appendData(dataSet, rows, 1280);
+});
+
+
 function startStream() {
 
 
@@ -52,34 +56,16 @@ function startStream() {
   var streamButton = document.getElementById("streamButton");
   streamButton.innerHTML = "Stop" + "\nstream";
 
-  // set interval of data stream
-  var myVar = setInterval(
-
-    // data streaming itself
-    function() {
-
-      // append data
-      dataSet.append({
-
-        // x value
-        x: "P" + indexSetter,
-
-        // random value from 1 to 500
-        value : Math.floor((Math.random() * 500)+ 1)
-      });
-
-      // removes first point
-      dataSet.remove(0);
-      indexSetter++;
-    }, 200            // interval
-  );
-
   streamButton.onclick = function (){
-    // clears interval which stops streaming
-    clearInterval(myVar);
-    streamButton.onclick = function () {
+    socket.emit('stop');
+    streamButton.onclick = function(){
       startStream();
     };
     streamButton.innerHTML = "Start" + "\nstream";
   };
 };
+
+
+
+// ln52 buffer data didn't receive
+// modify event listener to more callback
